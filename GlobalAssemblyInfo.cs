9f9de74cc98109e5397410665d2bf66241e9e5c3
Bastian Eicher <bastian@eicher.net>
2016-04-25 12:22:51 +0200

    Started development on v0.5.0.0

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("WebApi.Pagination")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("0.4.0.0")]
+[assembly: AssemblyVersion("0.5.0.0")]
 [assembly: ComVisible(false)]
 
 [assembly: InternalsVisibleTo("WebApi.Pagination.UnitTests, PublicKey=002400000480000094000000060200000024000052534131000400000100010065be9ae8ce335904900434943dae20813bffdcda0a7c2a95ca78d64d0b645f2403086e71824a26e62e4a0e45400c45cf24cdba5010ba8476c5af6a6e931622ff924283bed343b0d756c62264840d77d97789c6ad21b24ca180ae878beabd5f21ad835c6c92618d505fed0ffd973b98199b921cab78c69a42b85ac5467d3aefc8")]
