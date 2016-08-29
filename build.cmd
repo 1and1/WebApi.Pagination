aa0a1c190f7fc043a9e77929c32f8f17ac01a3dd
Bastian Eicher <bastian@eicher.net>
2016-08-29 11:05:02 +0200

    Tweaked build script

--- build.cmd
+++ build.cmd
@@ -12,14 +12,14 @@ if not defined VS140COMNTOOLS (
 )
 call "%VS140COMNTOOLS%vsvars32.bat"
 
-::Compile Visual Studio solution
+echo Compiling Visual Studio solution...
 nuget restore WebApi.Pagination.sln
-msbuild WebApi.Pagination.sln /nologo /t:Rebuild /p:Configuration=%BuildConfiguration%
+msbuild WebApi.Pagination.sln /nologo /v:q /m /t:Rebuild /p:Configuration=%BuildConfiguration%
 if errorlevel 1 pause
 
-::Create NuGet packages
+echo Creating NuGet packages...
 mkdir build\%BuildConfiguration%\Packages
-nuget pack WebApi.Pagination\WebApi.Pagination.csproj -Properties Configuration=%BuildConfiguration% -Symbols -OutputDirectory build\%BuildConfiguration%\Packages
+nuget pack WebApi.Pagination\WebApi.Pagination.csproj -Verbosity quiet -Properties Configuration=%BuildConfiguration% -Symbols -OutputDirectory build\%BuildConfiguration%\Packages
 if errorlevel 1 pause
 
 popd
